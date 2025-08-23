f38a07b889e1885a32b802fc9e6fa90fa6321d8b
Ranu Gupta <you@example.com>
2025-08-23 09:55:52 +0530

    i have update login

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,32 +1,107 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Login.css"
 const Login = () => {
+  const [FullName,setFullName] =useState("")
+  const [MobileNumber,setMobileNumber] =useState("")
+  const [Emailaddress,setEmailaddress]=useState("")
+  const [todo,settodo] =useState([])
+  const[msg,setmsg]=useState("")
+  const[check,setcheck]=useState("")
+  const[op,setop]=useState(true)
+   const[op1,setop1]=useState(false)
+
+
+
+  function Add(){
+    const q="@"
+    if(check.length==0){
+      setmsg("Please accept terms and condition")
+    }
+    else if(FullName.length==0){
+      setmsg("You have not filled your name")
+    }
+    else if(MobileNumber.length==0){
+      setmsg("You have not filled your no.")
+    }
+    else if(Emailaddress.length==0){
+      setmsg("You have not filled your email")
+    }
+    else if(Emailaddress==q){
+      setmsg("email is not applicable")
+    }
+    
+    else{
+      const as = [...todo,{FullName,MobileNumber,Emailaddress,check}]
+    settodo(as)
+    setmsg("Submitted")
+    }
+  }
+
+  
   return (
+    
     <div>
       
       <div className="logsign">
         <div className="login">
             <div className="sl">
                 <ul>
-                <a href="/Signup"><li>Signup</li></a>
-                <a href="/Login"><li>Login</li></a>
+                <li  onClick={()=>{setop(true), setop1(false)}}>Signup</li>
+                <li  onClick={()=>{setop(false), setop1(true)}}  >Login</li>
                 </ul>
             </div>
-            <h2>Signup</h2>
+
+            {/* ------------------- */}
+            {op&&<div className="sp">
+              <h2>Signup</h2>
             <h2>  Naresh IT account</h2>
             <div className="input">
-                <input type="text" placeholder='Full Name' />
-                <input type="number" placeholder='Mobile Number' />
-                <input type="email" placeholder='Email address'/>
+                <input type="text" placeholder='Full Name' onChange={(e)=>setFullName(e.target.value)} />
+                
+                <input type="number" placeholder='Mobile Number' onChange={(e)=>setMobileNumber(e.target.value)} />
+                <input type="email" placeholder='Email address' onChange={(e)=>setEmailaddress(e.target.value)}/>
             </div>
             <div className="checkbox">
-            <input type="checkbox" placeholder='' />I accept Naresh IT Terms of Use and Privacy Notice.
+            <input type="checkbox" placeholder='' onChange={(e)=> setcheck(e.target.value)}/>I accept Naresh IT Terms of Use and Privacy Notice.
+            
+            </div>
+            <div className="para">
+                            <p>{msg}</p>
 
             </div>
+
             <div className="button">
-            
-                                    <button>  Submit</button>
+                                    <button onClick={Add}>  Submit</button>
                                     </div>
+            </div>
+}
+{/* -----------login ----------------- */}
+{
+  op1&&<div className="lp">
+              <h2>Login</h2>
+            <h2>  Naresh IT account</h2>
+            <div className="input">
+
+              
+                <input type="mobile number" placeholder='Mobile Number' onChange={(e)=>setMobileNumber(e.target.value)} />
+                
+            </div>
+            
+            <div className="para">
+                            <p>{msg}</p>
+
+            </div>
+
+            <div className="button">
+                                    <button onClick={Add}>  Submit</button>
+                                    </div>
+            </div>
+}
+
+
+{/* -------------- */}
+
+
 
 
         </div>
