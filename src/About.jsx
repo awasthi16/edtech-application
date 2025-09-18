2459a031a894f9d58c345e9415019812598604fd
aditya sharma <sharmaaditya5465@gmail.com>
2025-09-18 10:20:10 +0530

    ranu aditya

--- src/About.jsx
+++ src/About.jsx
@@ -1,8 +1,109 @@
 import React from 'react'
+import "./About.css"
+import TrainersProfile from "./TrainersProfile"
 
+import OurTheme from './OurTheme'
+
+TrainersProfile
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div className="Zint0">
+<div className="image">
+           <img src="https://zintinstitute.com/wp-content/uploads/2022/08/logo-zint-png-format-2022.png" alt="" width={300} />
+        </div>
+    <div className="Zint1">
+      <h2>The New Way to <span className="span">success</span></h2>
+       <p>ZINT, Founder and Managing Director.</p>
+       <p>A recipient of “International Achievers Award for Education Excellence“</p>
+       <p>Zint is an entrepreneur armed with a noble vision to make a difference in the career aspirations of the students. 
+       20+ years of experience in the education sector, Zint is the founder and the driving force behind the victorious journey of Zint IT.</p> 
+    <button className="Zint2">ENQUIRE NOW ! </button>
+        
+    </div>
+  </div>
+
+  
+  <div className="div00">
+    <h2>Our journey</h2>
+    <p>All cards must be the same height and width for space calculations on large screens.</p>
+
+
+
+ <div className="div0">
+
+      <div className="div1">
+        <h4 className="year">2003</h4>
+        <div className="y1">
+          <h3>A Humble Beginning...</h3>
+            <p>Started in the year 2003 with an intention to provide a Just Opportunity to maximum Students who deserve a chance in the IT Industry!!</p>
+            <p>In a small campus of 4000 SFT</p>
+        
+        </div>
+      </div>
+
+      <div className="div2">
+        <h3 className="year">2018</h3>
+        <div className="y1">
+          <h3>15+ Years of Training Academic Students...</h3>
+            <p>Trained over 10 Lakh + Students.</p>
+            <p>From 1800+ Colleges.</p>
+            <p>Spanning across 16 States.</p>
+        </div>
+      </div>
+
+      <div className="div33">
+        <h4 className="year">2018</h4>
+        <div className="y1">
+          <h3>15+ Years of Training the Corporates...</h3>
+         
+            <p>Preferred Corporate Training</p>
+            <p>Partner for 100+ IT Majors in Hyderabad.</p>
+            <p>Provided Corporate Training in 400+ Corporates.</p>
+         
+        </div>
+      </div>
+
+   <div className="div4">
+        <h3 className="year">2018</h3>
+        <div className="y1">
+          <h3>15+ Years of Training Academic Students...</h3>
+            <p>Trained over 10 Lakh + Students.</p>
+            <p>From 1800+ Colleges.</p>
+            <p>Spanning across 16 States.</p>
+        </div>
+      </div>
+
+    </div>
+    <div></div>
+  
+  </div>
+
+
+ <div class=" lastdiv">
+    <div class="a1">
+     <img src=" https://s3.ap-south-1.amazonaws.com/nareshit.images/11443340.png" alt="" width={50}/>
+      <h3>Career-Focused Tech Academy</h3>
+      <p>Providing job-oriented training designed to boost your career prospects in the tech industry.</p>
+    </div>
+
+    <div class="a1">
+     <img src= "https://s3.ap-south-1.amazonaws.com/nareshit.images/11486993.png" alt="" width={50}/>
+      <h3>Guidance from Industry Experts</h3>
+      <p>Learn directly from experienced professionals who bring real-world expertise to your learning journey.</p>
+    </div>
+
+    <div class="a1">
+      <img src="https://s3.ap-south-1.amazonaws.com/nareshit.images/2490446.png" alt="" width={50} />
+      <h3>Comprehensive & Future-Ready Curriculum</h3>
+      <p>Stay ahead with an in-depth course structure that aligns with the latest industry trends and technologies.</p>
+    </div>
+  </div>
+
+<TrainersProfile></TrainersProfile>
+ <OurTheme></OurTheme>
+
+     </div>
   )
 }
 
---
+++ src/TrainersProfile.jsx
@@ -0,0 +1,105 @@
+import React from 'react'
+import "./TrainersProfile.css"
+
+const TrainersProfile = () => {
+  return (
+    <div>
+<div className="main-div">
+    <h2>Trainers Profile</h2>
+    <p>Explore new and trending free online courses.</p>
+
+    <div className="first-div">
+      <div className="trainer-card">
+        <div className="trainer-img">
+          <img src="https://s3.ap-south-1.amazonaws.com/nareshit.images/images/image_1712142972236.png" alt="" width={270}  height={200}/>
+        </div>
+        <div className="trainer-info">
+          <h3>Mr. Nataraj</h3>
+          <p>Java/J2EE Trainer</p>
+        </div>
+      </div>
+
+      <div className="trainer-card">
+        <div className="trainer-img">
+          <img src="https://s3.ap-south-1.amazonaws.com/nareshit.images/images/image_1712142972236.png" alt="" width={270}  height={200}/>
+        </div>
+        <div className="trainer-info">
+          <h3>Mr. K V Rao</h3>
+          <p>Python & Java Trainer</p>
+        </div>
+      </div>
+
+      <div className="trainer-card">
+        <div className="trainer-img">     
+         <img src="https://s3.ap-south-1.amazonaws.com/nareshit.images/images/image_1712142972236.png" alt="" width={270}  height={200}/>
+        </div>
+        <div className="trainer-info">
+          <h3>Mr. Sudhakar Sharma</h3>
+          <p>UI Full Stack Trainer</p>
+        </div>
+      </div>
+
+      <div className="trainer-card">
+        <div className="trainer-img">
+          <img src="https://s3.ap-south-1.amazonaws.com/nareshit.images/images/image_1712142972236.png" alt="" width={270}  height={200}/>
+        </div>
+        <div className="trainer-info">
+          <h3>Mr. Kishan B</h3>
+          <p>Java Trainer</p>
+        </div>
+      </div>
+    </div>
+
+
+
+
+     <div className="second-div">
+      <div className="trainer-card">
+        <div className="trainer-img">
+          <img src="https://s3.ap-south-1.amazonaws.com/nareshit.images/images/image_1712142972236.png" alt="" width={270}  height={200}/>
+        </div>
+        <div className="trainer-info">
+          <h3>Mr. Nataraj</h3>
+          <p>Java/J2EE Trainer</p>
+        </div>
+      </div>
+
+      <div className="trainer-card">
+        <div className="trainer-img">
+          <img src="https://s3.ap-south-1.amazonaws.com/nareshit.images/images/image_1712142972236.png" alt="" width={270}  height={200}/>
+        </div>
+        <div className="trainer-info">
+          <h3>Mr. K V Rao</h3>
+          <p>Python & Java Trainer</p>
+        </div>
+      </div>
+
+      <div className="trainer-card">
+        <div className="trainer-img">     
+         <img src="https://s3.ap-south-1.amazonaws.com/nareshit.images/images/image_1712142972236.png" alt="" width={270}  height={200}/>
+        </div>
+        <div className="trainer-info">
+          <h3>Mr. Sudhakar Sharma</h3>
+          <p>UI Full Stack Trainer</p>
+        </div>
+      </div>
+
+      <div className="trainer-card">
+        <div className="trainer-img">
+          <img src="https://s3.ap-south-1.amazonaws.com/nareshit.images/images/image_1712142972236.png" alt="" width={270}  height={200}/>
+        </div>
+        <div className="trainer-info">
+          <h3>Mr. Kishan B</h3>
+          <p>Java Trainer</p>
+        </div>
+      </div>
+    </div> 
+  </div>
+
+
+
+     </div>
+  )
+}
+
+export default TrainersProfile
