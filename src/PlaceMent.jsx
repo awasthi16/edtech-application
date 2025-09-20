dd600435f79c8087c9bdf967ef885fede306fcb6
RajveerKushwah <rajveerkushwah53579@gmail.com>
2025-09-20 07:21:28 +0530

    rajveer up

--- src/PlaceMent.jsx
+++ src/PlaceMent.jsx
@@ -7,27 +7,27 @@ const PlaceMent = () => {
         <>
 
             <header>
-                <div className="Services3div">
-                    <div className="Services3box">
-                        <div className="Services3box1">
-                            <div className="Services3div1">
+                <div className="Services3div1">
+                    <div className="Services3box1">
+                        <div className="Services3box11">
+                            <div className="Services3div11">
 
-                                <div className="Services3div1a">
+                                <div className="Services3div1a1">
                                     <p>Placement Registration</p>
                                 </div>
-                                <div className="Services3div1b">
-                                    <div className="Services3div1b1">
+                                <div className="Services3div1b1">
+                                    <div className="Services3div1b11">
                                         <img src="/image1(nareshit2).jpeg" alt="" />
 
                                     </div>
-                                    <div className="Services3div1b2">
+                                    <div className="Services3div1b21">
 
-                                        <div className="Services3div1b2a">
-                                            <div className="Services3div1b2a1">
+                                        <div className="Services3div1b2a1">
+                                            <div className="Services3div1b2a11">
                                                 <h4>
                                                     Navigating Placement-Oriented IT Courses</h4>
                                             </div>
-                                            <div className="Services3div1b2a2">
+                                            <div className="Services3div1b2a21">
                                                 <p>Embarking on a journey towards a successful IT career involves strategic skill development. In this programme, we'll guide you through the most relevant and placement-oriented courses in the IT sector. These programmes are designed to enhance your employability and provide the practical skills demanded by industry leaders.</p> <br />
                                                 <p>Begin your journey by understanding the current requirements of the IT industry. Explore the in-demand skills and technologies that employers are seeking in prospective candidates.</p>
                                                 <p>By completing these Programme, you'll have a comprehensive understanding of the courses that align with the demands of the IT job market. These placement-oriented courses will not only enhance your technical skills but also increase your employability in the dynamic and evolving IT sector. Best of luck in your pursuit of a rewarding IT career!</p>
@@ -51,7 +51,7 @@ const PlaceMent = () => {
 
 
 
-                            <div className="Services3div2a">
+                            <div className="Services3div2a1">
                                 <button>Enquiry</button>
                             </div>
                         </div>
@@ -62,8 +62,8 @@ const PlaceMent = () => {
 
 
             </header>
-<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-            <SevicesCommon/>
+            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+            <SevicesCommon />
         </>
     )
 }
