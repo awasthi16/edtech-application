ed2740d0e791cff0143ac627e0630e1f82604bb7
RajveerKushwah <rajveerkushwah53579@gmail.com>
2025-09-29 07:34:03 +0530

    rajveer

--- src/DashBoard.jsx
+++ src/DashBoard.jsx
@@ -64,9 +64,10 @@ const DashBoard = () => {
                       <li className={style.t1}>Total Students</li>
                       <div className={style.num}>
                         <li className={style.n1}>+12.5%</li>
-                        <li><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
+                        <li className={style.imgpro}><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
                           <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002-.014.002zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4" />
-                        </svg></li></div>
+                        </svg></li>
+                        </div>
                       <li className={style.per1}>vs last quarter</li>
                     </div>
 
@@ -75,7 +76,7 @@ const DashBoard = () => {
                       <li className={style.t1}>Revenue This Month</li>
                       <div className={style.num}>
                         <li className={style.n2}>$162K</li>
-                        <li><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-currency-dollar" viewBox="0 0 16 16">
+                        <li className={style.imgpro}><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-currency-dollar" viewBox="0 0 16 16">
                           <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73z" />
                         </svg></li></div>
                       <li className={style.per2}>+4.5% this month</li>
@@ -85,7 +86,7 @@ const DashBoard = () => {
                       <li className={style.t3}>Course Completion</li>
                       <div className={style.num}>
                         <li className={style.n3}>89%</li>
-                        <li><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-arrow-up-right" viewBox="0 0 16 16">
+                        <li className={style.imgpro}><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-arrow-up-right" viewBox="0 0 16 16">
                           <path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z" />
                         </svg></li></div>
                       <li className={style.per3}>Above target</li>
@@ -95,14 +96,14 @@ const DashBoard = () => {
                       <li className={style.t4}>Avg Class Hours</li>
                       <div className={style.num}>
                         <li className={style.n4}>28.5</li>
-                        <li><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-file-earmark-ruled" viewBox="0 0 16 16">
+                        <li className={style.imgpro}><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-file-earmark-ruled" viewBox="0 0 16 16">
                           <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V9H3V2a1 1 0 0 1 1-1h5.5zM3 12v-2h2v2zm0 1h2v2H4a1 1 0 0 1-1-1zm3 2v-2h7v1a1 1 0 0 1-1 1zm7-3H6v-2h7z" />
                         </svg></li></div>
                       <li className={style.per4}>per week</li>
-                    </div></div> </div></div> <br />
+                    </div></div> </div></div>
               <hr />
               <div className={style.chatmaindiv}>
-                <div className={style.chatdiv}> 
+                <div className={style.chatdiv}>
                   <div className={style.chatdiv1}></div>
                   <div className={style.chatdiv2}></div>
                   <div className={style.chatdiv3}></div>
@@ -144,7 +145,7 @@ const DashBoard = () => {
                             <li className={style.t1}>Total Students</li>
                             <div className={style.num}>
                               <li className={style.n1}>+12.5%</li>
-                              <li><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
+                              <li  className={style.imgpro}><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
                                 <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002-.014.002zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4" />
                               </svg></li></div>
                             <li className={style.per1}>vs last quarter</li>
@@ -154,7 +155,7 @@ const DashBoard = () => {
                             <li className={style.t2}>Revenue This Month</li>
                             <div className={style.num}>
                               <li className={style.n2}>$162K</li>
-                              <li><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-currency-dollar" viewBox="0 0 16 16">
+                              <li  className={style.imgpro}><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-currency-dollar" viewBox="0 0 16 16">
                                 <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73z" />
                               </svg></li></div>
                             <li className={style.per2}>+4.5% this month</li>
@@ -164,7 +165,7 @@ const DashBoard = () => {
                             <li className={style.t3}>Course Completion</li>
                             <div className={style.num}>
                               <li className={style.n3}>89%</li>
-                              <li><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-arrow-up-right" viewBox="0 0 16 16">
+                              <li  className={style.imgpro}><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-arrow-up-right" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z" />
                               </svg></li></div>
                             <li className={style.per3}>Above target</li>
@@ -174,7 +175,7 @@ const DashBoard = () => {
                             <li className={style.t4}>Avg Class Hours</li>
                             <div className={style.num}>
                               <li className={style.n4}>28.5</li>
-                              <li><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-file-earmark-ruled" viewBox="0 0 16 16">
+                              <li  className={style.imgpro}><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-file-earmark-ruled" viewBox="0 0 16 16">
                                 <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V9H3V2a1 1 0 0 1 1-1h5.5zM3 12v-2h2v2zm0 1h2v2H4a1 1 0 0 1-1-1zm3 2v-2h7v1a1 1 0 0 1-1 1zm7-3H6v-2h7z" />
                               </svg></li></div>
                             <li className={style.per4}>per week</li>
@@ -223,7 +224,7 @@ const DashBoard = () => {
                     <li className={style.ass}>Assignee: Finance Team</li><br />
                     <div className={style.newstudent}>
                       <li className={style.newen}>Faculty performance review</li>
-                      <li className={style.en1}>medium</li>
+                      <li className={style.en11}>medium</li>
                     </div>
                     <li className={style.john}>Due: 2024-07-10</li>
                     <li className={style.ass} >Assignee: HR Manager</li><br />
@@ -231,11 +232,11 @@ const DashBoard = () => {
                       <li className={style.newen}>System backup verification</li>
                       <li className={style.en1}>high</li>
                     </div>
-                    <li className={style.john}  >Due: 2024-07-03</li>+
+                    <li className={style.john}  >Due: 2024-07-03</li>
                     <li className={style.ass}>Assignee: IT Team</li><br />
                     <div className={style.newstudent} >
                       <li  className={style.newen}>Course catalog update</li>
-                      <li className={style.en1}>low</li>
+                      <li className={style.en11}>low</li>
                     </div>
                     <li className={style.john}>Due: 2024-07-15</li>
                     <li className={style.ass}>Assignee: Academic Team</li><br /><br />
@@ -250,12 +251,12 @@ const DashBoard = () => {
                   <li className={style.manaa}>Manage user access and permissions</li>
                   <div className={style.userRole}>
                     <div className={style.user1}>
-                      <div className={style.add}> <li className={style.admin}>Administrator</li><li className={style.u3}>3 users</li> </div>
+                      <div className={style.add}> <li className={style.admin}>Administrator</li><li className={style.u31}>3 users</li> </div>
                       <li className={style.manaaa}>Full Access</li>
                       <button className={style.viewbtn}>View Users</button> <button className={style.editper}>Edit Permissions</button>
                     </div>
                     <div className={style.user2}>
-                      <div className={style.add}> <li className={style.admin}>Faculty</li><li className={style.u3}>89 users</li> </div>
+                      <div className={style.add}> <li className={style.admin}>Faculty</li><li className={style.u32}>89 users</li> </div>
                       <li className={style.manaaa}>Teaching Access</li>
                       <button className={style.viewbtn}>View Users</button> <button className={style.editper}>Edit Permissions</button>
                     </div>
@@ -265,10 +266,10 @@ const DashBoard = () => {
                       <button className={style.viewbtn}>View Users</button> <button className={style.editper}>Edit Permissions</button>
                     </div>
                     <div className={style.user4}>
-                      <div className={style.add}> <li className={style.admin}>Student</li><li className={style.u3}>99 users</li> </div>
+                      <div className={style.add}> <li className={style.admin}>Student</li><li className={style.u33}>99 users</li> </div>
                       <li className={style.manaaa}>Student Access</li>
                       <button className={style.viewbtn}>View Users</button> <button className={style.editper}>Edit Permissions</button>
-                    </div> 
+                    </div>
                   </div>
                 </div>
               </div>
@@ -356,7 +357,7 @@ const DashBoard = () => {
               <li className={style.t}>Total Students</li>
               <div className={style.num}>
                 <li className={style.n}>1,247</li>
-                <li><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
+                <li className={style.imgpro}><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
                   <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002-.014.002zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4" />
                 </svg></li></div>
               <li className={style.per}>+12%</li>
@@ -367,7 +368,7 @@ const DashBoard = () => {
               <li className={style.t}>Revenue This Month</li>
               <div className={style.num}>
                 <li className={style.n}>$162K</li>
-                <li><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-currency-dollar" viewBox="0 0 16 16">
+                <li className={style.imgpro}><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-currency-dollar" viewBox="0 0 16 16">
                   <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73z" />
                 </svg></li></div>
               <li className={style.per}>+4.5%</li>
@@ -377,7 +378,7 @@ const DashBoard = () => {
               <li className={style.t}>Course Completion</li>
               <div className={style.num}>
                 <li className={style.n}>89%</li>
-                <li><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-arrow-up-right" viewBox="0 0 16 16">
+                <li className={style.imgpro}><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-arrow-up-right" viewBox="0 0 16 16">
                   <path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z" />
                 </svg></li></div>
               <li className={style.per}>+2.1%</li>
@@ -387,7 +388,7 @@ const DashBoard = () => {
               <li className={style.t}>Faculty Efficiency</li>
               <div className={style.num}>
                 <li className={style.n}>94%</li>
-                <li><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-file-earmark-ruled" viewBox="0 0 16 16">
+                <li className={style.imgpro}><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-file-earmark-ruled" viewBox="0 0 16 16">
                   <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V9H3V2a1 1 0 0 1 1-1h5.5zM3 12v-2h2v2zm0 1h2v2H4a1 1 0 0 1-1-1zm3 2v-2h7v1a1 1 0 0 1-1 1zm7-3H6v-2h7z" />
                 </svg></li></div>
               <li className={style.per}>+1.8%</li>
